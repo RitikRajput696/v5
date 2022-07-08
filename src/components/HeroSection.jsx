9a6c0a3c02a63738b587a6a2b0ad36fc86811793
ritikrajput696 <rixusinput696@gmail.com>
2022-07-08 16:56:05 +0530

    created navbar

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function HeroSection() {
+  return (
+    <section className="hero container">
+      <article className="hero-text">
+        <p>Hii, MY Name is</p>
+        <h1>Ritik I am a </h1>
+        <h1>Frontend Web Developer</h1>
+      </article>
+      <div className="hero-img"></div>
+    </section>
+  );
+}
+
+export default HeroSection;
