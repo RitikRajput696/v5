1b11709e16e27a3025951c5e18682a907db644bd
ritikrajput696 <rixusinput696@gmail.com>
2022-07-09 13:46:43 +0530

    added contact section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import ProjectSection from "./components/ProjectSection";
+import ContactSection from "./components/ContactSection";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <HeroSection />
       <AboutSection />
       <ProjectSection />
+      <ContactSection />
     </>
   );
 }
---
+++ src/components/ContactSection.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+function ContactSection() {
+  return (
+    <section className="ContactSection container">
+      <div className="contact-container">
+        <div>
+          <span className="text-green">04.</span>
+          <span className="mid-heading">What &#8217;s Next?</span>
+        </div>
+
+        <h1>Get in Touch</h1>
+        <p className="form-para">
+          I’m currently looking for any new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I’ll try my
+          best to get back to you!
+        </p>
+        <form
+          action="https://formspree.io/f/moqrlnbz"
+          method="post"
+          id="form"
+          className="form"
+        >
+          <div>
+            <label for="Full Name">Full Name</label>
+            <input
+              type="text"
+              value=""
+              id="fullname"
+              name="fullName"
+              placeholder="John Wick"
+              required
+            />
+          </div>
+          <div>
+            <label for="Email">Your Email</label>
+            <input
+              type="email"
+              value=""
+              id="email"
+              name="email"
+              placeholder="abc@example.com"
+              required
+            />
+          </div>
+          <div>
+            <label for="Message">Message</label>
+            <textarea
+              name="Message"
+              id="message"
+              cols="30"
+              rows="10"
+              placeholder="Say hello"
+              required
+            ></textarea>
+          </div>
+          <button className="button" type="submit">
+            Send Message
+          </button>
+        </form>
+      </div>
+      <input type="text" />
+    </section>
+  );
+}
+
+export default ContactSection;
